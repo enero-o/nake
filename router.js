4a970b8b737e54792b0797fe92465d48cf6681e6
Akerele Victor <eneroakerele@gmail.com>
2019-06-14 17:01:07 +0100

    dynamic active headers

--- router.js
+++ router.js
@@ -2,10 +2,14 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/', (req, res) => res.render('index'));
-router.get('/gallery', (req, res) => res.render('gallery'));
-router.get('/offers', (req, res) => res.render('offers'));
-router.get('/shops', (req, res) => res.render('shops'));
-router.get('/events', (req, res) => res.render('events'));
+router.get('/', (req, res) => res.render('index', { homeIsActive: 'active', title: 'Home' }));
+
+router.get('/gallery', (req, res) => res.render('gallery', { galleryIsActive: 'active', title: 'Gallery' }));
+
+router.get('/offers', (req, res) => res.render('offers', { offersIsActive: 'active', title: 'Offers' }));
+
+router.get('/shops', (req, res) => res.render('shops', { shopIsActive: 'active', title: 'Shops' }));
+
+router.get('/events', (req, res) => res.render('events', { eventsIsActive: 'active', title: 'Events' }));
 
 module.exports = router;
