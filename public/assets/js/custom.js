0e666656fa5ae3c31f49fba300fb3a107a1c20f9
Akerele Victor <eneroakerele@gmail.com>
2019-06-30 19:09:19 +0100

    slider fix

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -2,14 +2,21 @@
  * Plugin Core Init
  * For DEMO purposes only. Extract what you need.
  * ============================================================ */
- 
+
 'use strict';
 
-$(document).ready(function() {
-
+$(document).ready(function () {
 	/*
 		Write your custom JS here
 	*/
-    
-
+  //Intialize Slider
+  //Intialize Slider
+  const slider = new Swiper('#demo-hero-3 .swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    parallax: true,
+    speed: 1000
+  });
 });
